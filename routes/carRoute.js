f645bc3020108d995d3a21aacb33a535d5566093
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-05-08 22:29:38 +0200

    Old but gold

--- routes/carRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { addCar, updateCar } = require("./../controllers/items/carController");
-const router = express.Router();
-
-router.route("/").post(addCar);
-router.route("/:offerId").patch(updateCar);
-
-module.exports = router;
