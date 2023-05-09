8f0d7a1f50c884833155b24b3c69655dccea4252
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-05-09 21:46:12 +0200

    removed old code

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const glabalErrorHandler = require("./controllers/errorController");
-const carRoute = require("./routes/carRoute");
-const accessoryRoute = require("./routes/accessoryRoute");
-const motorcycleRoute = require("./routes/motorcycleRoute");
-const scooterRoute = require("./routes/scooterRoute");
 const offerRoute = require("./routes/offerRoute");
 
 const app = express();
@@ -13,10 +9,6 @@ app.use(cors());
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
-app.use("/car", carRoute);
-app.use("/accessory", accessoryRoute);
-app.use("/motorcycle", motorcycleRoute);
-app.use("/scooter", scooterRoute);
 app.use("/offer", offerRoute);
 
 app.use(glabalErrorHandler);
