4b1be24f95d9f5b80aad4bac4a09e1ffea5f15a0
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-05-24 01:10:54 +0200

    env

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,7 +3,7 @@ const catchAsync = require("./../utils/catchAsync");
 const User = require("./../models/userModel");
 const AppError = require("../utils/appError");
 
-const maxAge = 3 * 24 * 60 * 60;
+const maxAge = process.env.JWT_EXPIRE_DAY * 24 * 60 * 60 * 1000;
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: maxAge,
@@ -13,7 +13,7 @@ const createToken = (id) => {
 exports.signUp = catchAsync(async (req, res, next) => {
   const user = await User.create(req.body);
   const token = createToken(user._id);
-  res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+  res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge });
   res.status(200).json({
     status: "success",
     data: user,
@@ -26,7 +26,7 @@ exports.login = catchAsync(async (req, res, next) => {
   const user = await User.login(email, password);
   if (!user) return next(new AppError("Incorrect username or password"));
   const token = createToken(user._id);
-  res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+  res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge });
   res.status(200).json({
     status: "success",
     data: user,
@@ -58,13 +58,14 @@ exports.isLoggedIn = catchAsync(async (req, res, next) => {
 exports.protect = catchAsync(async (req, res, next) => {
   const token = req.cookies.jwt;
   if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decodedToken) => {
       if (err) {
         console.log(err.message);
         return next(new AppError("You are not logged in", 403));
       } else {
-        req.user = decodedToken;
-        console.log(decodedToken);
+        const user = await User.findById(decodedToken.id);
+        req.user = user;
+        console.log(req.user);
         return next();
       }
     });
