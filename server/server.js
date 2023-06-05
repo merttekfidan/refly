0aa22babfe76fbc790e716c469ebbaa5eee379eb
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-06-05 22:58:58 +0200

    Change port

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ mongoose
     console.log("DB is on");
   });
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log("Server is on");
 });
