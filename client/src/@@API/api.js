fc4dd52bd4c8c16dca21ef2b9e2545403c81db84
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-06-07 23:37:18 +0200

    Create Test API

--- client/src/@@API/api.js
+++ client/src/@@API/api.js
@@ -1,11 +1,47 @@
 import axios from "axios";
 
-const loginApi = async (email, password) => {
+export const loginApi = async (email, password) => {
   try {
-    const response = await axios.post("http://127.0.0.1:8000/user/login", {
-      email: email,
-      password: password,
-    });
+    const response = await axios.post(
+      "http://127.0.0.1:8000/user/login",
+      {
+        email: email,
+        password: password,
+      },
+      { withCredentials: true }
+    );
+
+    // Return the response data after successful login
+    return response.data;
+  } catch (error) {
+    // Handle errors
+    throw error;
+  }
+};
+
+export const logoutApi = async () => {
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/user/logout",
+      null,
+      {
+        withCredentials: true,
+      }
+    );
+
+    // Return the response data after successful logout
+    return response.data;
+  } catch (error) {
+    // Handle errors
+    throw error;
+  }
+};
+export const myBusiness = async () => {
+  try {
+    const response = await axios.get(
+      "http://127.0.0.1:8000/business/myBusiness",
+      { withCredentials: true }
+    );
 
     // Return the response data after successful login
     return response.data;
