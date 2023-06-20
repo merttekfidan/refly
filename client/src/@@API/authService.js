855ee477430a7783ecdb5a3d1bf5ef2f78959bc8
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-06-20 23:45:24 +0200

    add AuthService

--- client/src/@@API/authService.js
+++ client/src/@@API/authService.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const API_URL = "/user/";
+
+// Register
 const register = async (userData) => {
   const response = await axios.post(API_URL, userData);
   if (response.data) {
@@ -9,8 +11,25 @@ const register = async (userData) => {
   return response.data;
 };
 
+// login
+const login = async (userData) => {
+  const response = await axios.post(API_URL + "login", userData);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+// Logout
+const logout = async () => {
+  localStorage.removeItem("user");
+  await axios.post(API_URL + "logout");
+};
+
 const authService = {
   register,
+  login,
+  logout,
 };
 
 export default authService;
