7f221c7006e1ce955dded519018ee6392658c1cc
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-08-09 01:19:58 +0200

    Bug fix - init empty token

--- client/src/@@API/offerService.js
+++ client/src/@@API/offerService.js
@@ -1,16 +1,36 @@
 import axios from "axios";
 
 const FORM_URL = "/offer/";
-const { token } = JSON.parse(localStorage.getItem("user"));
-const config = {
-  headers: { Authorization: `Bearer ${token}` },
-};
+let token = "";
+token = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : "";
 export const submitListForm = async (formData) => {
   try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
     const submittedList = await axios.post(FORM_URL, formData, config);
     //console.log(submittedList);
     return submittedList;
   } catch (error) {
-    throw error.response.data.error;
+    throw error;
+  }
+};
+export const submitImages = async (formData, id) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "multipart/form-data",
+      },
+    };
+    const submittedList = await axios.patch(FORM_URL + id, formData, config);
+    //console.log(submittedList);
+    return submittedList;
+  } catch (error) {
+    throw error;
   }
 };
