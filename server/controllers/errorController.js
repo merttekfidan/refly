00f374109c4804f7803f79352c634e12362c0d6a
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-08-12 16:09:15 +0200

    Error handling

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,23 +1,70 @@
-const AppError = require("./../utils/appError");
+const AppError = require("../utils/appError");
 
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+const handleDuplicateErrorDB = (err) => {
+  const message = `Duplicate field value: ${
+    Object.values(err.keyValue)[0]
+  }. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data ${errors.join(". ")}`;
+  return new AppError(message, 400);
+};
+const handleJWTError = () => {
+  return new AppError("Invalid token. Please log in again", 401);
+};
+const handleJWTExpiredError = () => {
+  return new AppError("Your token has expired! Please log in again", 401);
+};
+
+const sendErrorDev = (err, req, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
 const sendErrorProd = (err, req, res) => {
+  // A)Operational , trusted error: send message to client
   if (err.isOperational) {
     return res.status(err.statusCode).json({
       status: err.status,
       message: err.message,
     });
-  } else {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-      error: err,
-    });
+    // B) Programming or other unknown error : don't leak error details
   }
+  // 1) Log Error
+  console.error("Errorrr", err);
+  // 2) Send generic meesage
+  return res.status(500).json({
+    status: "error",
+    message: "Something went wrong",
+  });
 };
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
-  let error = { ...err, name: err.name, message: err.message };
-  sendErrorProd(error, req, res);
+  if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, req, res);
+  } else if (process.env.NODE_ENV === "production") {
+    //Interestingly, you have to define ''name'' property seperately. Otherwise, object destructering will not catch err.name
+    let error = { ...err, name: err.name };
+    error.message = err.message;
+    if (error.name === "CastError") error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateErrorDB(error);
+    if (error.name === "ValidationError")
+      error = handleValidationErrorDB(error);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+    sendErrorProd(error, req, res);
+  }
 };
