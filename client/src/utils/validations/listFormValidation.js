b164fb52b0f887053352bae455badef2ef5cafa1
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-08-15 19:28:17 +0200

    fix validation

--- client/src/utils/validations/listFormValidation.js
+++ client/src/utils/validations/listFormValidation.js
@@ -1,32 +1,10 @@
-// Helper function to check if a value is empty
-function isEmpty(value) {
-  return !value || value.trim() === "" || value === "0";
-}
-
-// Helper function to check if a value is a valid number
-function isNumber(value) {
-  return !isNaN(value);
-}
-
-function containsOnlyDigits(value) {
-  const numberRegex = /^\d+$/;
-  return numberRegex.test(value);
-}
-
-// Helper function to check if a value is within a specified range
-function isInRange(value, min, max) {
-  return value >= min && value <= max;
-}
-
-// Helper function to check if a URL is valid
-function isValidUrl(url) {
-  try {
-    new URL(url);
-    return true;
-  } catch (error) {
-    return false;
-  }
-}
+import {
+  isEmpty,
+  isNumber,
+  containsOnlyDigits,
+  isInRange,
+  isValidUrl,
+} from "./validationFunctions";
 
 // Validation schema for the form
 const validationSchema = {
