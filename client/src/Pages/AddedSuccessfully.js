ea23cc7d40c5aaeeb09d91d2b9c5c2d78b58a070
Merdjan Tekfidan <merttekfidan@gmail.com>
2023-08-15 19:29:20 +0200

    fix class to classname

--- client/src/Pages/AddedSuccessfully.js
+++ client/src/Pages/AddedSuccessfully.js
@@ -2,28 +2,28 @@ import { Link } from "react-router-dom";
 
 function AddedSuccessfully() {
   return (
-    <section class="booking-confirm-area section-padding position-relative">
-      <span class="circle-bg circle-bg-1 position-absolute"></span>
-      <span class="circle-bg circle-bg-2 position-absolute"></span>
-      <span class="circle-bg circle-bg-3 position-absolute"></span>
-      <span class="circle-bg circle-bg-4 position-absolute"></span>
-      <span class="circle-bg circle-bg-5 position-absolute"></span>
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="booking-confirm-content text-center">
-              <i class="la la-check-circle-o font-size-90 text-success"></i>
-              <div class="section-heading pt-3">
-                <h2 class="sec__title mb-2">
+    <section className="booking-confirm-area section-padding position-relative">
+      <span className="circle-bg circle-bg-1 position-absolute"></span>
+      <span className="circle-bg circle-bg-2 position-absolute"></span>
+      <span className="circle-bg circle-bg-3 position-absolute"></span>
+      <span className="circle-bg circle-bg-4 position-absolute"></span>
+      <span className="circle-bg circle-bg-5 position-absolute"></span>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-12">
+            <div className="booking-confirm-content text-center">
+              <i className="la la-check-circle-o font-size-90 text-success"></i>
+              <div className="section-heading pt-3">
+                <h2 className="sec__title mb-2">
                   Thank you. Your offer has been added.
                 </h2>
-                <p class="sec__desc">
+                <p className="sec__desc">
                   We'll check your offer and publish as soon as possible
                 </p>
               </div>
-              <div class="btn-box padding-top-30px">
-                <Link to="/" class="btn-gray btn-gray-lg px-4">
-                  Go to Homepage <i class="la la-arrow-right ml-1"></i>
+              <div className="btn-box padding-top-30px">
+                <Link to="/" className="btn-gray btn-gray-lg px-4">
+                  Go to Homepage <i className="la la-arrow-right ml-1"></i>
                 </Link>
               </div>
             </div>
